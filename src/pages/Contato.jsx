9d48bd1a3db9eadc527ea2f23464c61c9262a258
Jusley Arley <jusleyarley@mini-de-jusley.getinfo.corp>
2018-06-26 11:40:46 -0300

    Página de Contato com envio dos emails

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Button} from 'bloomer';
+import SimpleReactValidator from 'simple-react-validator';
+import axios from 'axios';
 
-//Componentes
-import Button from '../general/Button';
+//Icones
+import { CheckCircleIcon, CloseCircleIcon, CubeSendIcon } from 'mdi-react';
 
 //Imagem
 import xicara from '../assets/image/contato/xicara.png';
@@ -10,7 +13,51 @@ import xicara from '../assets/image/contato/xicara.png';
 import '../assets/styles/pages/contato.css';
 
 class Contato extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      form: props.form || {name: '', subject: '', message: ''},
+      answer: ''
+    };
+
+    this.validator = new SimpleReactValidator();
+  }
+
+  handleFormChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({form:{...this.state.form, [name]: value}})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({answer: ""});
+
+    if(!this.validator.allValid()) {
+      this.validator.showMessages();
+      this.forceUpdate();
+      return;
+    }
+
+    this.setState({answer: [<CubeSendIcon />, "Enviando mensagem..."]});
+
+    axios.post("", {
+      name: this.state.form.name,
+      email: this.state.form.email,
+      subject: this.state.form.subject,
+      message: this.state.form.message
+    })
+    .then((response) => {
+      this.setState({answer:[<CheckCircleIcon/>, "Sua mensagem foi enviada com sucesso!"]});
+    })
+    .catch((error) => {
+      this.setState({answer: [<CloseCircleIcon/>, "Ops! Ocorreu algum erro ao enviar a mensagem. Por favor, tente novamente."]})
+    });
+  }
+
   render(){
+    const {form} = this.state;
     return(
       <div id="contato"> 
         <div className="section">
@@ -19,63 +66,97 @@ class Contato extends React.Component {
               <div className="column is-8">
                 <p className="titulo">Entre em contato</p>
                 <div className="box-contato">
-                  <div className="field is-horizontal espaco top">
-                    <div className="field-label is-normal">
-                      <label className="label">Nome</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <input className="input" type="text" placeholder="Nome"/>
-                        </p>
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="field is-horizontal espaco top">
+                      <div className="field-label is-normal">
+                        <label className="label">Nome</label>
+                      </div>
+                      <div className="field-body">
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              name="name"
+                              className="input"
+                              type="text"
+                              placeholder="Nome"
+                              value={form.name}
+                              onChange={this.handleFormChange}
+                            />
+                            <span>{this.validator.message('name', form.name, 'required', false, {default: "Campo obrigatório"})}</span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="field is-horizontal espaco">
-                    <div className="field-label is-normal">
-                      <label className="label">E-mail</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <input className="input" type="text" placeholder="E-mail"/>
-                        </p>
+                    <div className="field is-horizontal espaco">
+                      <div className="field-label is-normal">
+                        <label className="label">E-mail</label>
+                      </div>
+                      <div className="field-body">
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              name="email"
+                              className="input"
+                              type="email"
+                              placeholder="E-mail"
+                              value={form.email}
+                              onChange={this.handleFormChange}
+                            />
+                            <span>{this.validator.message('email', form.email, 'required', false, {default: "Campo obrigatório"})}</span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="field is-horizontal espaco">
-                    <div className="field-label is-normal">
-                      <label className="label">Assunto</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <input className="input" type="text" placeholder="Assunto"/>
-                        </p>
+                    <div className="field is-horizontal espaco">
+                      <div className="field-label is-normal">
+                        <label className="label">Assunto</label>
+                      </div>
+                      <div className="field-body">
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              name="subject"
+                              className="input"
+                              type="text"
+                              placeholder="Assunto"
+                              value={form.subject}
+                              onChange={this.handleFormChange}
+                            />
+                            <span>{this.validator.message('subject', form.subject, 'required', false, {default: "Campo obrigatório"})}</span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="field is-horizontal espaco">
-                    <div className="field-label is-normal">
-                      <label className="label">Mensagem</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <textarea className="textarea" placeholder="Mensagem..."></textarea>
-                        </p>
+                    <div className="field is-horizontal espaco">
+                      <div className="field-label is-normal">
+                        <label className="label">Mensagem</label>
+                      </div>
+                      <div className="field-body">
+                        <div className="field">
+                          <p className="control">
+                            <textarea
+                              name="message"
+                              className="textarea"
+                              placeholder="Mensagem..."
+                              value={form.message}
+                              onChange={this.handleFormChange}>
+                            </textarea>
+                            <span>{this.validator.message('message', form.message, 'required', false, {default: "Campo obrigatório"})}</span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-
-                  <div className="field">
-                    <div className="control">
-                      <div className="button-espaco"><Button link="">Enviar</Button></div>
+                    
+                    <div className="answer">{this.state.answer}</div>
+                    <div className="field">
+                      <div className="control">
+                        <div className="button-espaco"><Button type="submit">Enviar</Button></div>
+                      </div>
                     </div>
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>

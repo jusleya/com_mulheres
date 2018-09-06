ecccf0df92f4060532dbaae686ace45900c75b52
Jusley Arley <jusley@getinfo.net.br>
2018-09-06 13:12:15 -0300

    Add de eventos anteriores

---
+++ src/views/Eventos/components/Anteriores.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Button   from '../../../components/Button';
+
+//Image
+import womakers from '../image/womakers.jpg';
+
+const Anteriores = () => (
+  <div id="anteriores">
+    <div className="container">
+      <p className="titulo">Eventos Anteriores</p>
+      <div className="columns">
+        <div align="center" className="column is-4 box-right container-column">
+          <p className="titulo-evento">WomakersCode Summit Nordeste</p>
+          <img className="image" src={womakers} alt=""/>
+          <p className="descricao">
+            O Evento <i>WomakersCode</i> ocorreu no dia 18 de agosto de 2018, que teve foco no desenvolvimento web e mobile.
+          </p>
+          <br/>
+          <div align="center"><Button type="" link="/">Saiba Mais<i className="material-icons">keyboard_arrow_right</i></Button></div>
+        </div>
+        <div align="center" className="column is-4 box-right container-column">
+          <p className="titulo-evento">WomakersCode Summit Nordeste</p>
+            <img className="image" src={womakers} alt=""/>
+            <p className="descricao">
+              O Evento <i>WomakersCode</i> ocorreu no dia 18 de agosto de 2018, que teve foco no desenvolvimento web e mobile.
+            </p>
+            <br/>
+            <div align="center"><Button type="" link="/">Saiba Mais<i className="material-icons">keyboard_arrow_right</i></Button></div>
+        </div>
+        <div align="center" className="column is-4 container-column">
+          <p className="titulo-evento">WomakersCode Summit Nordeste</p>
+            <img className="image" src={womakers} alt=""/>
+            <p className="descricao">
+              O Evento <i>WomakersCode</i> ocorreu no dia 18 de agosto de 2018, que teve foco no desenvolvimento web e mobile.
+            </p>
+            <br/>
+            <div align="center"><Button type="" link="/">Saiba Mais<i className="material-icons">keyboard_arrow_right</i></Button></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Anteriores;
--- src/views/Eventos/index.jsx
+++ src/views/Eventos/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 //Components
+import Anteriores from './components/Anteriores';
 import Destaque from './components/Destaque';
 
 //CSS
 import './css/style.css';
+
+const hr = {border:"1px dashed rgb(189, 203, 216)"};
 
 const Eventos = () => (
   <div id="eventos">
@@ -12,6 +15,23 @@ const Eventos = () => (
       <div className="container">
         <div className="box-card">
           <Destaque/>
+        </div>
+        <hr style={hr}/>
+      </div>
+      <Anteriores/>
+      <div className="container paginas">
+        <div className="columns is-centered">
+          <div className="column is-6">
+            <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+              <a class="pagination-previous">Previous</a>
+              <a class="pagination-next">Next page</a>
+              <ul class="pagination-list">
+                <li><a class="pagination-link" aria-label="Goto page 1" aria-current="page">1</a></li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
